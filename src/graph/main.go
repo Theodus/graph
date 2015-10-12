4fb91ae60276281c98a689f7c66ec0dc45de2fe6
Theodus <theodusbutler@gmail.com>
2015-10-12 13:10:24 -0400

    Update main.go

--- src/graph/main.go
+++ src/graph/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
-		fmt.Println("Please specify the Yaml file for me to use.")
+		fmt.Println("Please specify the JSON file for me to use.")
 		return
 	} else if len(args) > 1 {
 		fmt.Println("Too many arguments! I can only use one file at a time.")
